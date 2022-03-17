83fa08995dc8551f059cf17df61ccc3b7ea2d48c
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-03-17 12:33:16 +0530

    Generate md-tables for inputs and outputs

--- src/action.ts
+++ src/action.ts
@@ -1,12 +1,43 @@
 //  Library
 import * as core from '@actions/core'
+import { createMarkdownTable, readActionYaml } from './helpers'
 
 // =============
 // GITHUB ACTION
 // =============
 
 export async function action() {
-    core.notice('Hello World!')
+
+    //  Get Action Metadata
+    const metadata = await readActionYaml()
+
+    //  Generate inputs-md-table
+    if (metadata.inputs) {
+        const inputs = []
+        for (const [key, value] of Object.entries(metadata.inputs)) {
+            inputs.push([
+                key,
+                value.description,
+                value.required ? '**required**' : `\`${value.default?.toString()}\`` || ''
+            ])
+            const inputsMD = createMarkdownTable(inputs, ['input', 'description', 'default'])
+            core.setOutput('inputs-md-table', inputsMD)
+        }
+    }
+
+    //  Generate outputs-md-table
+    if (metadata.outputs) {
+        const outputs = []
+        for (const [key, value] of Object.entries(metadata.outputs)) {
+            outputs.push([
+                key,
+                value.description
+            ])
+        }
+        const outputsMD = createMarkdownTable(outputs, ['outputs', 'description'])
+        core.setOutput('outputs-md-table', outputsMD)
+    }
+
 }
 
 //  -----------------
--- src/helpers/readActionYaml.ts
+++ src/helpers/readActionYaml.ts
@@ -3,9 +3,12 @@ import * as fs from 'node:fs'
 import * as jsYaml from 'js-yaml'
 import { path } from '../config'
 
+//  Type Definitions
+import type { Action } from '../types'
+
 /** Read action.yaml file from the workspace */
-export async function readActionYaml<T>(target: string = path) {
+export async function readActionYaml(target: string = path): Promise<Action> {
     const contents = await fs.promises.readFile(target, { encoding: 'utf-8' })
     const yaml = jsYaml.load(contents)
-    return yaml as T
+    return yaml as Action
 }
