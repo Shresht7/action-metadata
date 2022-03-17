974b6ec1a39b12295390cc9b2203a0c294a52e70
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-03-17 12:16:56 +0530

    Create type defnitions

---
+++ src/types/index.ts
@@ -0,0 +1,29 @@
+//  ================
+//  TYPE DEFINITIONS
+//  ================
+
+interface ActionInput {
+    description: string,
+    required?: boolean,
+    default?: string
+}
+
+interface ActionOutput {
+    description: string
+}
+
+export interface Action {
+    name: string,
+    author: string,
+    description: string,
+    runs: {
+        using: string,
+        main: string,
+    },
+    branding?: {
+        icon?: string,
+        color?: string
+    },
+    inputs?: Record<string, ActionInput>
+    outputs?: Record<string, ActionOutput>
+}
